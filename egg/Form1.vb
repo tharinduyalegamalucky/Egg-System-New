a6ba45cd6a9042650c8d58646273f6964e81fabf
Tharindu Yalegama <87438005+tharinduyalegamalucky@users.noreply.github.com>
2022-07-22 10:57:04 +0530

    second commit

--- egg/Form1.vb
+++ egg/Form1.vb
@@ -1,12 +1,57 @@
-Imports System.Data.SqlClient
 Imports System.Data
-
+Imports System.Data.SqlClient
+Imports System.IO
 
 Public Class Form1
 
+    'Dim con As New SqlConnection("Data Source=LAPTOP-DCJ10BPM;Initial Catalog=Egg;Integrated Security=True")
     Dim table1 As New DataTable
 
+    Dim con As New SqlConnection
+    Dim cmd As New SqlCommand
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Dim query As String = "insert into egg values(@SANo,@Company,@Location,@cudate,@Remark,@Eggcategory,@Qty)"
+        Using con As SqlConnection = New SqlConnection("Data Source=LAPTOP-DCJ10BPM;Initial Catalog=Egg;Integrated Security=True")
+            Using cmd As SqlCommand = New SqlCommand(query, con)
+                cmd.Parameters.AddWithValue("SANo", TextSANo.Text)
+                cmd.Parameters.AddWithValue("Company", ComboCompany.Text)
+                cmd.Parameters.AddWithValue("Location", ComboLocation.Text)
+                cmd.Parameters.AddWithValue("cudate", dpt.Text)
+                cmd.Parameters.AddWithValue("Remark", dpt.Text)
+                cmd.Parameters.AddWithValue("Eggcategory", ComboEggCategory.Text)
+                cmd.Parameters.AddWithValue("Qty", TextQty.Text)
 
+                con.Open()
+                'cmd.ExecuteNonQuery()
+                con.Close()
+            End Using
+        End Using
+        MessageBox.Show("Data Addedd")
+
+    End Sub
+
+    Private Sub data()
+        Dim query As String = "select * from egg"
+        Using con As SqlConnection = New SqlConnection("Data Source=LAPTOP-DCJ10BPM;Initial Catalog=Egg;Integrated Security=True")
+            Using cmd As SqlCommand = New SqlCommand(query, con)
+                Using da As New SqlDataAdapter
+                    da.SelectCommand = cmd
+                    Using dt As New DataTable()
+                        da.Fill(dt)
+                        DataGridView1.DataSource = dt
+                    End Using
+                End Using
+            End Using
+        End Using
+    End Sub
+
+    Private Function getDataLists(selectQuery As String) As Object
+        Throw New NotImplementedException()
+    End Function
+
+    Private Sub ExecuteQuery(addQuery As String)
+        Throw New NotImplementedException()
+    End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         table1.Columns.Add("SA No", Type.GetType("System.Int32"))
@@ -18,6 +63,10 @@ Public Class Form1
 
         DataGridView1.DataSource = table1
 
+        If con.State = ConnectionState.Open Then
+            con.Close()
+        End If
+
     End Sub
 
     Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
@@ -28,18 +77,12 @@ Public Class Form1
         DataGridView1.DataSource = table1
     End Sub
 
+    'To Close Btn
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Me.Close()
     End Sub
 
-    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles btnSave.Click
-        Dim con As SqlConnection = New SqlConnection("Data Source=LAPTOP-DCJ10BPM;Initial Catalog=Egg;User Id=sa;Password=000000;Integrated Security=True")
-        Dim cmd As SqlCommand = New SqlCommand("select * from Egg where SANo='" & TextSANo.Text & "' and Company='" & ComboCompany.Text & "' and Location='" & ComboLocation.Text & "' and cudate='" & dpt1.Text & "' and Remark='" & TextRemark.Text & "' and Eggcategory= '" & ComboEggCategory.Text & "' and Qty='" & TextQty.Text & "'", con)
-        Dim sda As SqlDataAdapter = New SqlDataAdapter(cmd)
-        Dim dt As DataTable = New DataTable()
-        sda.Fill(dt)
-        MessageBox.Show("Successfully Added", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
-    End Sub
+
 
     Private Sub Form1_TextChanged(sender As Object, e As EventArgs) Handles MyBase.TextChanged
         Dim query As String = "SELECT * FROM egg WHERE FIRST"
@@ -54,4 +97,8 @@ Public Class Form1
         ComboEggCategory.Text = ""
     End Sub
 
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles dpt.TextChanged
+        Dim todaysdate As String = String.Format("{0:dd/MM/yyyy}", DateTime.Now)
+        dpt.Text = todaysdate
+    End Sub
 End Class
