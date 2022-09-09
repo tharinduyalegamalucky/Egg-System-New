ecdafcafd04193d7bf6e464f2119deb51e3b2f81
Tharindu Yalegama <87438005+tharinduyalegamalucky@users.noreply.github.com>
2022-09-09 12:58:05 +0530

    Update Form1.vb

--- egg/Form1.vb
+++ egg/Form1.vb
@@ -1,4 +1,6 @@
 Imports System.Data.SqlClient
+Imports egg.DBConnection
+
 
 
 Public Class Form1
@@ -6,245 +8,62 @@ Public Class Form1
     Dim table1 As New DataTable
     Dim con As New SqlConnection
     Dim query As String
+    Dim DT As DataTable
+    Dim DS As DataRow
 
 
     'DataGridView Details-----
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         DBConnection.connect()
 
-        table1.Columns.Add("Egg Category", Type.GetType("System.String"))
-        table1.Columns.Add("Qty", Type.GetType("System.String"))
-        DataGridView1.DataSource = table1
-        If con.State = ConnectionState.Open Then
-            con.Close()
-        End If
+        ' egg category load ------
+        Dim SqlStrings As String
+        SqlStrings = "SELECT [Cat_Id],[Cat_Name],[SysDateTime] FROM [dbo].[Egg Category Tbl]"
+        DT = DBConnection.GenerateResultSet(SqlStrings)
+        For i As Integer = 0 To DT.Rows.Count - 1
+            'MessageBox.Show(DT.Rows(i).Item(0).ToString)
+            ComboEggCategory.Items.Add(DT.Rows(i).Item(1).ToString)
+            ComboEggCategory.AutoCompleteCustomSource.Add(DT.Rows(i).Item(0).ToString)
+        Next
 
+        '''' combo load'''
+        Dim SqlString As String
+        SqlString = "SELECT [com_id],[com_name],[SysDateTime] FROM [dbo].[Company Tbl]"
+        DT = DBConnection.GenerateResultSet(SqlString)
+        For i As Integer = 0 To DT.Rows.Count - 1
+            ComboCompany.Items.Add(DT.Rows(i).Item(1).ToString)
+            ComboCompany.AutoCompleteCustomSource.Add(DT.Rows(i).Item(0).ToString)
+        Next
 
-        '----ComboCompany Items------
-
-        ComboCompany.Items.Add("FARMS PRIDE (PVT) LTD")
-        ComboCompany.Items.Add("MIDLAND BREEDERS (PVT) LTD")
-        ComboCompany.Items.Add("GOLDEN GRAINS (PVT) LTD")
-        ComboCompany.Items.Add("HARE PARK DAIRY (PVT) LTD")
-        ComboCompany.Items.Add("CONSOLIDATED")
-        ComboCompany.Items.Add("ALTRA GUARD")
-        ComboCompany.Items.Add("WAYAMBA DEVELOPMENT (PVT) LTD")
-        ComboCompany.Items.Add("FORTUNE AGRO INDUSTRIES (PVT) LTD")
-        ComboCompany.Items.Add("CASUAL WAGES - PPL")
-        ComboCompany.Items.Add("CRYSBRO AUTO SERVICE")
 
     End Sub
 
     Private Sub ComboCompany_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboCompany.SelectedIndexChanged
-        ComboLocation.Items.Clear()
 
-        'Company Locations
-
-        If ComboCompany.SelectedItem = "FARMS PRIDE (PVT) LTD" Then
-
-            ComboLocation.Items.Add("CONSOLIDATED  ")
-            ComboLocation.Items.Add("NORTH ")
-            ComboLocation.Items.Add("CENTRAL")
-            ComboLocation.Items.Add("UVA & EAST")
-            ComboLocation.Items.Add("HABARANA AREA")
-            ComboLocation.Items.Add("IPOLOGAMA AREA")
-            ComboLocation.Items.Add("GAMPOLA AREA")
-            ComboLocation.Items.Add("MATALE AREA")
-            ComboLocation.Items.Add("MAHIYANGANAYA AREA")
-            ComboLocation.Items.Add("DEHIATTHAKANDIYA AREA")
-            ComboLocation.Items.Add("CG - NORTH CENTRAL")
-            ComboLocation.Items.Add("GANEWALPOLA")
-            ComboLocation.Items.Add("PADIKARAMADUWA - 1")
-            ComboLocation.Items.Add("NEKATHUNAWEWA")
-            ComboLocation.Items.Add("NETHIYAGAMA")
-            ComboLocation.Items.Add("IPOLOGAMA - 1")
-            ComboLocation.Items.Add("EPPAWALA")
-            ComboLocation.Items.Add("SELESTIMADUWA")
-            ComboLocation.Items.Add("GALKULAMA - 1")
-            ComboLocation.Items.Add("VILACHCHIYA")
-            ComboLocation.Items.Add("GAMPOLA - 1")
-            ComboLocation.Items.Add("DAMBULLA")
-            ComboLocation.Items.Add("BAKAMOONA - 1")
-            ComboLocation.Items.Add("MAPAKADAWEWA - 1")
-            ComboLocation.Items.Add("BIBILE - 1")
-            ComboLocation.Items.Add("SADUNPURA - 1")
-            ComboLocation.Items.Add("BANDANAGALA - 1")
-            ComboLocation.Items.Add("MADIRIGIRIYA")
-            ComboLocation.Items.Add("GAMPOLA - GATE")
-            ComboLocation.Items.Add("PPL - GATE")
-            ComboLocation.Items.Add("BATTARAMULLA - GATE")
-            ComboLocation.Items.Add("BALAPITIYA - GATE")
-            ComboLocation.Items.Add("PUPURESSA REGION")
-            ComboLocation.Items.Add("PUPURESSA AREA")
-            ComboLocation.Items.Add("PUPURESSA")
-            ComboLocation.Items.Add("HABARANA - GATE")
-            ComboLocation.Items.Add("MAPAKADAWEWA - GATE")
-            ComboLocation.Items.Add("PROCESSING PLANT")
-            ComboLocation.Items.Add("DAMBULLA - GATE")
-            ComboLocation.Items.Add("PUPURESSA - GATE")
-            ComboLocation.Items.Add("SADUNPURA - GATE")
-            ComboLocation.Items.Add("TRANSPORT")
-            ComboLocation.Items.Add("GAMPOLA - HO")
-            ComboLocation.Items.Add("BALAPITIYA - 1")
-            ComboLocation.Items.Add("FARMS PRIDE")
-            ComboLocation.Items.Add("CG-CENTRAL")
-            ComboLocation.Items.Add("TESS AGRO - GATE")
-            ComboLocation.Items.Add("BATTARAMULLA - STO")
-            ComboLocation.Items.Add("LAB")
-            ComboLocation.Items.Add("FINLAY GATE")
-            ComboLocation.Items.Add("GOLDEN GRAINS PROJECT")
-            ComboLocation.Items.Add("GALGAMUWA")
-            ComboLocation.Items.Add("GALGAMUWA (WD)")
-            ComboLocation.Items.Add("GAMPOLA - FMG")
-            ComboLocation.Items.Add("FPP - STORE")
-            ComboLocation.Items.Add("GAMPOLA - 1 (WD)")
-            ComboLocation.Items.Add("SISILCO - GATE")
-            ComboLocation.Items.Add("SBO")
-            ComboLocation.Items.Add("KADUWELA - GATE")
-            ComboLocation.Items.Add("ULHITIYA_GATE")
-            ComboLocation.Items.Add("ULHITIYA")
-            ComboLocation.Items.Add("GG-MAH")
-            ComboLocation.Items.Add("FAIRLINE")
-            ComboLocation.Items.Add("GAMPOLA (TECH)")
-            ComboLocation.Items.Add("KOSMO FARM")
-            ComboLocation.Items.Add("WARIYAPOLA")
-            ComboLocation.Items.Add("MILAN FARM")
-            ComboLocation.Items.Add("Colombo 01 ")
-            ComboLocation.Items.Add("Colombo 02")
-            ComboLocation.Items.Add("Central 01")
-            ComboLocation.Items.Add("Central 02")
-            ComboLocation.Items.Add("Down South")
-            ComboLocation.Items.Add("Institution")
-            ComboLocation.Items.Add("HR & Admin")
-            ComboLocation.Items.Add("Finance")
-            ComboLocation.Items.Add("IT")
-            ComboLocation.Items.Add("Management")
-            ComboLocation.Items.Add("KOSMO - GATE")
-            ComboLocation.Items.Add("KOSMO")
-            ComboLocation.Items.Add("TRANSPORT")
-
-
-        ElseIf ComboCompany.SelectedItem = "MIDLAND BREEDERS (PVT) LTD" Then
-
-            ComboLocation.Items.Add("BREEDER")
-            ComboLocation.Items.Add("NAWALAPITIYA")
-            ComboLocation.Items.Add("PARAGALA")
-            ComboLocation.Items.Add("FAIRLINE")
-            ComboLocation.Items.Add("HATCHERY")
-            ComboLocation.Items.Add("FAIRLENE - GATE")
-            ComboLocation.Items.Add("PARAGALA - GATE")
-            ComboLocation.Items.Add("NAWALAPITIYA - GATE")
-            ComboLocation.Items.Add("HATCHERY - GATE")
-            ComboLocation.Items.Add("LAB")
-            ComboLocation.Items.Add("MIDLAND BREEDERS")
-            ComboLocation.Items.Add("GAMPOLA - 1")
-            ComboLocation.Items.Add("GAMPOLA - 1")
-            ComboLocation.Items.Add("GAMPOLA - HO")
-            ComboLocation.Items.Add("TRANSPORT")
-            ComboLocation.Items.Add("PPL")
-            ComboLocation.Items.Add("GALGAMUWA")
-            ComboLocation.Items.Add("GOLDEN GRAINS-NC")
-
-        ElseIf ComboCompany.SelectedItem = "GOLDEN GRAINS (PVT) LTD" Then
-            ComboLocation.Items.Add("GOLDEN GRAINS")
-            ComboLocation.Items.Add("MAHIYANGANA")
-            ComboLocation.Items.Add("KANTHALE")
-            ComboLocation.Items.Add("MAHIYANGANA(GG) - GATE")
-            ComboLocation.Items.Add("KANTHALE(GG) - GATE")
-            ComboLocation.Items.Add("GAMPOLA - 1")
-            ComboLocation.Items.Add("GAMPOLA - HO")
-            ComboLocation.Items.Add("GAMPOLA - FMG")
-            ComboLocation.Items.Add("HEBARAWA - GATE")
-            ComboLocation.Items.Add("TRANSPORT")
-
-        ElseIf ComboCompany.SelectedItem = "HARE PARK DAIRY (PVT) LTD" Then
-            ComboLocation.Items.Add("GALABODA")
-            ComboLocation.Items.Add("HARE PARK DAIRY (PVT) LTD")
-
-        ElseIf ComboCompany.SelectedItem = "CONSOLIDATED" Then
-            ComboLocation.Items.Add("MAIN STORES")
-            ComboLocation.Items.Add("WAYAMBA")
-            ComboLocation.Items.Add("WAYAMBA AREA")
-            ComboLocation.Items.Add("GALGAMUWA - GATE")
-            ComboLocation.Items.Add("PPL - 2")
-
-        ElseIf ComboCompany.SelectedItem = "ALTRA GUARD" Then
-            ComboLocation.Items.Add("ALTRA GUARD")
-
-        ElseIf ComboCompany.SelectedItem = "WAYAMBA DEVELOPMENT (PVT) LTD" Then
-            ComboLocation.Items.Add("GAMPOLA - HO ")
-            ComboLocation.Items.Add("LAB")
-            ComboLocation.Items.Add("TRANSPORT")
-
-        ElseIf ComboCompany.SelectedItem = "FORTUNE AGRO INDUSTRIES (PVT) LTD" Then
-            ComboLocation.Items.Add("FORTUNE AGRO INDUSTRIES")
-            ComboLocation.Items.Add("KURUNAGALA AREA")
-            ComboLocation.Items.Add("FORTUNE AGRO")
-            ComboLocation.Items.Add("FORTUNE AGRO - GATE")
-            ComboLocation.Items.Add("GAMPOLA - HO")
-            ComboLocation.Items.Add("FORTUNE AGRO INDUSTRIES(PVT) LTD")
-            ComboLocation.Items.Add("FORTUNE-GP INDUSTRIES")
-            ComboLocation.Items.Add("GALEWELA")
-            ComboLocation.Items.Add("GP HATCHERY")
-            ComboLocation.Items.Add("MONARAGALA AREA")
-            ComboLocation.Items.Add("FORTUNE AGRO - (SYD)")
-            ComboLocation.Items.Add("SIYAMBALANDUWA_GATE")
-            ComboLocation.Items.Add("ANURADHAPURA AREA")
-            ComboLocation.Items.Add("FORTUNE AGRO - (THALAWA)")
-            ComboLocation.Items.Add("THALAWA_GATE")
-            ComboLocation.Items.Add("GP CHICKS")
-
-        ElseIf ComboCompany.SelectedItem = "CASUAL WAGES - PPL" Then
-            ComboLocation.Items.Add("")
-
-        ElseIf ComboCompany.SelectedItem = "CRYSBRO AUTO SERVICE" Then
-            ComboLocation.Items.Add("")
+        If ComboCompany.SelectedIndex <> -1 Then
+            ComboLocation.Items.Clear()
+            Dim SqlString As String
+            SqlString = "SELECT [Loc_id],[Loc_Name],[SysDateTime],[com_id] FROM [dbo].[Location Tbl] WHERE com_id ='" + ComboCompany.AutoCompleteCustomSource(ComboCompany.SelectedIndex) + "'   "
+            DT = DBConnection.GenerateResultSet(SqlString)
+            For i As Integer = 0 To DT.Rows.Count - 1
+                'MessageBox.Show(DT.Rows(i).Item(0).ToString)
+                ComboLocation.Items.Add(DT.Rows(i).Item(1).ToString)
+                ComboLocation.AutoCompleteCustomSource.Add(DT.Rows(i).Item(0).ToString)
+            Next
         End If
 
-        'Company ID
 
-        If ComboCompany.Text = "FARMS PRIDE (PVT) LTD" Then
-            cid.Text = 1
-
-        ElseIf ComboCompany.Text = "MIDLAND BREEDERS (PVT) LTD" Then
-            cid.Text = 2
-
-        ElseIf ComboCompany.Text = "GOLDEN GRAINS (PVT) LTD" Then
-            cid.Text = 3
-
-        ElseIf ComboCompany.Text = "HARE PARK DAIRY (PVT) LTD" Then
-            cid.Text = 4
-
-        ElseIf ComboCompany.Text = "CONSOLIDATED" Then
-            cid.Text = 5
-
-        ElseIf ComboCompany.Text = "ALTRA GUARD" Then
-            cid.Text = 6
-
-        ElseIf ComboCompany.Text = "WAYAMBA DEVELOPMENT (PVT) LTD" Then
-            cid.Text = 7
-
-        ElseIf ComboCompany.Text = "FORTUNE AGRO INDUSTRIES (PVT) LTD" Then
-            cid.Text = 8
-
-        ElseIf ComboCompany.Text = "CASUAL WAGES - PPL" Then
-            cid.Text = 9
-
-        ElseIf ComboCompany.Text = "CRYSBRO AUTO SERVICE" Then
-            cid.Text = 10
-
-        End If
     End Sub
 
 
     'save button
     'Insert,Save Query
-    'Date & DataBae Query
+    'Date & DataBase Query
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles btnSave.Click
 
         Dim getDate As Date = Date.Now
         Dim addDate As String = getDate.ToString(" yyyy-MM-dd ")
-        query = "INSERT into egg values ((Select ISNULL(MAX(SANo)+1,1) From egg),'" & addDate & "','" & ComboCompany.Text & "','" & ComboLocation.Text & "','" & TextRemark.Text & "','" & cid.Text & "')"
+        query = "INSERT into Stock Header Tbl values ((Select ISNULL(MAX(SA_No)+1,1) From Stock Header Tbl),'" & ComboLocation.Text & "','" & ComboCompany.Text & "','" & TextRemark.Text & "','" & addDate & "')"
         If (DBConnection.ExecuteSql(query)) Then
             MsgBox("Save To Details")
         End If
@@ -255,10 +74,35 @@ Public Class Form1
     'EggCategory DataBase Details
     Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
 
-        table1.Rows.Add(ComboEggCategory.Text.Trim, TextQty.Text.Trim)
-        DataGridView1.DataSource = table1
+        ' Message Box-------
+        If ComboCompany.Text = "" Then
+            MessageBox.Show("Not Selected the Company")
+            Exit Sub
+        End If
 
-        query = "INSERT into EggCategory values('" & ComboEggCategory.Text & "','" & TextQty.Text & "')"
+        If ComboLocation.Text = "" Then
+            MessageBox.Show("Not Selected the Company Location")
+            Exit Sub
+        End If
+
+        If TextRemark.Text = "" Then
+            MessageBox.Show("Not Completed the Remark ")
+            Exit Sub
+        End If
+
+        If ComboEggCategory.Text = "" Then
+            MessageBox.Show("Not Selected the Egg Category")
+            Exit Sub
+        End If
+
+        If TextQty.Text = "" Then
+            MessageBox.Show("Qty Can't be 0 or blank")
+            Exit Sub
+        End If
+
+        DataGridView1.Rows.Add(ComboEggCategory.Text, TextQty.Text)
+
+        query = "INSERT INTO [dbo].[Stock Details Tbl] VALUES ( '" & ComboEggCategory.Text & "', '" & TextQty.Text & "')"
         If (DBConnection.ExecuteSql(query)) Then
         End If
     End Sub
@@ -272,14 +116,17 @@ Public Class Form1
     'New Button----------
     'Clear to All Details
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        'Clear ------
+
+        ComboCompany.SelectedIndex = -1
+        ComboLocation.SelectedIndex = -1
+        ComboEggCategory.SelectedIndex = -1
         TextSANo.Text = ""
         TextRemark.Text = ""
         TextQty.Text = ""
-        ComboCompany.Text = ""
-        ComboLocation.Text = ""
-        ComboEggCategory.Text = ""
-        cid.Text = ""
     End Sub
+
 
 
 End Class
